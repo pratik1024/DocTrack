a416cefebfd8e6c08e2d69703a7ff1b479f231bb
pratik1024 <pratikdash1024@gmail.com>
2023-03-06 11:19:10 +0530

    Test1

--- app/src/main/java/com/example/doctrack/MainActivity3.java
+++ app/src/main/java/com/example/doctrack/MainActivity3.java
@@ -2,13 +2,38 @@ package com.example.doctrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        EditText mUsernameEditText = findViewById(R.id.editTextPhone);
+        EditText mPasswordEditText = findViewById(R.id.editTextTextPassword);
+        Button mLoginButton = findViewById(R.id.button3);
+        Button mLoginButton1 = findViewById(R.id.button4);
+        // Set up a click listener for the login button
+        mLoginButton.setOnClickListener(v -> {
+            // Check if the username and password are correct
+            if (mUsernameEditText.getText().toString().equals("123456") &&
+                    mPasswordEditText.getText().toString().equals("password")) {
+                // If the credentials are correct, navigate to the main activity
+                Intent intent = new Intent(MainActivity3.this, MainActivity6.class);
+                startActivity(intent);
+            } else {
+                // If the credentials are incorrect, display an error message
+                Toast.makeText(MainActivity3.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mLoginButton1.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+            });
     }
 }
---
+++ app/src/main/java/com/example/doctrack/MainActivity4.java
@@ -0,0 +1,27 @@
+package com.example.doctrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+
+    }
+}
